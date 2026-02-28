ac6ad29e47a2bde405cbc37ee5e49085535df99e
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2026-02-28 14:06:38 +0800

    oke

--- app/View/Components/PublicLayout.php
+++ app/View/Components/PublicLayout.php
@@ -7,6 +7,18 @@ use Illuminate\View\View;
 
 class PublicLayout extends Component
 {
+    // Daftarkan variabel title
+    public $title;
+
+    /**
+     * Create a new component instance.
+     * Kita beri nilai default jika halaman tidak mengirimkan title
+     */
+    public function __construct($title = 'Karyantara Solution - Web & Mobile App Development')
+    {
+        $this->title = $title;
+    }
+
     /**
      * Get the view / contents that represents the component.
      */
