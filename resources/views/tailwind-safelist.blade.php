3b0f177b6ba1be3be603fb74e6de41a5deb86941
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2026-05-07 18:26:08 +0800

    kode inject tailwind assets

---
+++ resources/views/tailwind-safelist.blade.php
@@ -0,0 +1,22 @@
+<div class="hidden">
+    <span class="bg-amber-100 text-amber-700"></span>
+    <span class="bg-red-100 text-red-700"></span>
+    <span class="bg-blue-100 text-blue-700"></span>
+    <span class="bg-indigo-100 text-indigo-700"></span>
+    <span class="bg-emerald-100 text-emerald-700"></span>
+    <span class="bg-orange-100 text-orange-700"></span>
+    <span class="bg-slate-100 text-slate-700"></span>
+    <span class="bg-[#1E293B] text-amber-500"></span>
+    <span class="bg-teal-100 text-teal-700"></span>
+
+    <span class="bg-red-800 text-red-100"></span>
+    <span class="bg-slate-800 text-slate-100"></span>
+    <span class="bg-emerald-800 text-emerald-100"></span>
+
+    <span class="bg-purple-100 text-purple-700"></span>
+
+    <span class="bg-rose-100 text-rose-700"></span>
+    <span class="bg-[#1E293B] text-white"></span>
+    <span class="bg-slate-800 text-amber-400"></span>
+    <span class="bg-gray-100 text-gray-700"></span>
+</div>
